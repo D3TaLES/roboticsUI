0e4d12ca01690d43cc313f5d8e741949371ff505
robotics_pc <d3tales@gmail.com>
2024-09-11 16:21:43 -0400

    sphinx debugging

--- docs_src/mkdocs.sh
+++ docs_src/mkdocs.sh
@@ -8,17 +8,20 @@ $env:MKDOCS_DIR="C:\Users\Lab\D3talesRobotics\roboticsUI\docs_src"
 cd $env:MKDOCS_DIR
 cd ../
 rm -r docs/
+mkdir $env:MKDOCS_DIR/_temp
 
-sphinx-apidoc -H "$env:projectname" -A "$env:author" -V $env:version --full -o ./docs ./robotics_api
-cp $env:MKDOCS_DIR/conf.py docs/
-cp $env:MKDOCS_DIR/index.rst ./docs/
-cp $env:MKDOCS_DIR/*.md ./docs/
-cp $env:MKDOCS_DIR/*.png ./docs/
+sphinx-apidoc -H "$env:projectname" -A "$env:author" -V $env:version --full -o $env:MKDOCS_DIR/_temp ./robotics_api
+cp $env:MKDOCS_DIR/conf.py $env:MKDOCS_DIR/_temp
+cp $env:MKDOCS_DIR/index.rst $env:MKDOCS_DIR/_temp
+cp $env:MKDOCS_DIR/*.md $env:MKDOCS_DIR/_temp
+cp $env:MKDOCS_DIR/*.png $env:MKDOCS_DIR/_temp
 
-cd docs/
+cd $env:MKDOCS_DIR/_temp
 make clean
 make html
 
+mkdir docs
+cp -r
 touch _build/html/.nojekyll
 cd $env:MKDOCS_DIR
 

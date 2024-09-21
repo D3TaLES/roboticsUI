1eb24adaa93da4c18c488c950de8c8c42fc4ba92
Rebekah Duke <rduke199@gmail.com>
2024-09-21 12:23:48 -0400

    branch from reb_dev

---
+++ robotics_api/snapshots/__init__.py
@@ -0,0 +1,7 @@
+"""
+The snapshots directory contains Kinova snapshots for various locations on the robotics work table.
+The snapshot naming is designed so that the robotics_api can access locations for vials and stations
+easily. If anything on the robotics work table is moved, these snapshots must be adjusted. The easiest
+way to adjust the snapshots is through the Kinova web UI. If the robot is on and connected to the
+workstation, go to http://192.168.1.10/ to access the Kinova web UI.
+"""
